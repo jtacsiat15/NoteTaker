a0993c1ff43f94cc1d834371e359d338ed14f2be
JCAASSHH <jalentacsiat@gmail.com>
2019-11-03 18:17:33 -0800

    commit before pull

--- app/src/main/java/com/example/notetaker/NoteActivity.java
+++ app/src/main/java/com/example/notetaker/NoteActivity.java
@@ -31,7 +31,6 @@ public class NoteActivity extends AppCompatActivity {
             String type = intent.getStringExtra("type");
             noteTitle.setText(title);
             content.setText(noteContent);
-            //noteType.
         }
 
         noteType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
