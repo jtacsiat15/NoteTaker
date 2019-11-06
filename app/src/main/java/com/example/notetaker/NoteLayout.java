c541a59d77274d086e29b59a4de4440a632f4025
JCAASSHH <jalentacsiat@gmail.com>
2019-11-05 19:15:20 -0800

    NoteActivityLayout

--- app/src/main/java/com/example/notetaker/NoteLayout.java
+++ app/src/main/java/com/example/notetaker/NoteLayout.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.icu.text.CaseMap;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,39 +21,47 @@ public class NoteLayout extends GridLayout {
         setColumnCount(3);
 
         GridLayout.LayoutParams layoutParams = new LayoutParams();
-        layoutParams.rowSpec = GridLayout.spec(0, 1, 1);
+        layoutParams.rowSpec = GridLayout.spec(1, 1, 1);
         layoutParams.columnSpec = GridLayout.spec(0, 3, 1);
 
         //creates title editText
         final EditText noteTitle = new EditText(context);
-        noteTitle.setLayoutParams(new GridLayout.LayoutParams(GridLayout.spec(0, 1, 1), GridLayout.spec(0, 2, 1)));
+        noteTitle.setLayoutParams(new GridLayout.LayoutParams(GridLayout.spec(0, 1, 1/2), GridLayout.spec(0, 2, 30)));
         noteTitle.setHint("Title");
         noteTitle.setId(R.id.noteTitle);
+        noteTitle.setGravity(Gravity.TOP);
         addView(noteTitle);
 
         //adds spinner type
         final Spinner noteType = new Spinner(context);
-        noteType.setLayoutParams(new GridLayout.LayoutParams(GridLayout.spec(0, 1, 1), GridLayout.spec(2, 1, 1)));
+        noteType.setLayoutParams(new GridLayout.LayoutParams(GridLayout.spec(0, 1, 1/2), GridLayout.spec(2, 1, 1)));
         noteType.setId(R.id.noteType);
         ArrayAdapter<CharSequence> spinnerArrayAdapter = ArrayAdapter.createFromResource(
                 context,
                 R.array.noteTypesArray,
                 android.R.layout.simple_spinner_item);
         noteType.setAdapter(spinnerArrayAdapter);
+        noteType.setGravity(Gravity.TOP);
         addView(noteType);
 
         //adds content to take notes with
         final EditText content = new EditText(context);
         content.setLayoutParams(layoutParams);
         content.setId(R.id.noteContent);
+        content.setGravity(Gravity.TOP);
 
         content.setHint("Content");
         addView(content);
 
         //adds done button
         Button doneButton = new Button(context);
+        GridLayout.LayoutParams buttonParams = new LayoutParams();
+        buttonParams.rowSpec = GridLayout.spec(2, 1, 1/2);
+        buttonParams.columnSpec = GridLayout.spec(0, 3, 1);
+        doneButton.setLayoutParams(buttonParams);
         content.setLayoutParams(layoutParams);
         doneButton.setId(R.id.doneButton);
+        doneButton.setGravity(Gravity.BOTTOM);
         doneButton.setText("Done");
         addView(doneButton);
     }
